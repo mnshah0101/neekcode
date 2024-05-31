67c9913ba75b64239e0ff7d6c57c1abf84c6812b
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-05-31 12:03:54 -0400

    favicon 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "@next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const Electrica = localFont({
   src: [
@@ -29,7 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
 
       <body className={Electrica.className}>{children}</body>
     </html>
